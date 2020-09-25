c5dd5a09ffced69a5c9efb29392c78c6680ac0fe
Ceasar Gomez <ceasargomez023@gmail.com>
2020-09-24 23:08:03 -0500

    Made changes where at least two integers are required and made changes to the error message.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,15 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide at least two integers");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+	int sum = Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+	for(int i = 2; i < args.length; i++)	//Create for loop to loop for at least two arguments
+		sum += Integer.valueOf(args[0]);
+		return sum;
+
     }
 }
